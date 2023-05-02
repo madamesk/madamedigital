ed20f3620224d0499899628ed31acf9fdc01a657
madamesk <131755354+madamesk@users.noreply.github.com>
2023-05-02 09:35:35 +0200

    Delete mulriplizieren.py

--- mulriplizieren.py
+++
@@ -1,6 +0,0 @@
-area = 0 #hier wird die Variable zur int festgelegt
-la = input("Eingabe der Länge des Recxhtecks: ")
-br = input("Eingabe der Breite: ")
-#Berechnung Flächeninhalt = Länge * Breite
-area = int(la) * int(br)
-print("%dm²" % area)
